629c9bc0b6230626ff6ba28382844a2e2fab5769
techrebbe <mmkaplan@gmail.com>
2026-07-26 09:04:56 +0300

    Register RTL Reader toolbar entry

---
+++ overlay/index.js
@@ -0,0 +1,25 @@
+import {AppRegistry, Image} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {PluginManager} from 'sn-plugin-lib';
+
+const RTL_READER_BUTTON_ID = 100;
+const icon = Image.resolveAssetSource(require('./assets/icon.png')).uri;
+
+AppRegistry.registerComponent(appName, () => App);
+PluginManager.init();
+
+PluginManager.registerButton(1, ['DOC'], {
+  id: RTL_READER_BUTTON_ID,
+  name: 'RTL Reader',
+  icon,
+  showType: 1,
+});
+
+PluginManager.registerButtonListener({
+  onButtonPress: event => {
+    if (event?.id === RTL_READER_BUTTON_ID) {
+      console.log('RTL_READER_OPEN');
+    }
+  },
+});
